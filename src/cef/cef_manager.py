b41ff6757f4c8c0b53eb3c39b00eba44115fe3b2
dzikoysk <dzikoysk@dzikoysk.net>
2018-02-12 14:09:41 +0100

    Improve cef_manager

--- src/cef/cef_manager.py
+++ src/cef/cef_manager.py
@@ -1,5 +1,6 @@
 from cefpython3 import cefpython as cef
 
+import platform
 
 class CefManager:
 
@@ -7,7 +8,11 @@ class CefManager:
         self.browsers = dict()
 
     def initialize(self):
+        self.check_versions()
         cef.Initialize()
+        self.create_browser(identifier='1', url='https://google.pl/')
+        self.create_browser(identifier='2', url='https://dzikoysk.net/')
+        self.keep_alive()
 
     def keep_alive(self):
         cef.MessageLoop()
@@ -16,13 +21,19 @@ class CefManager:
     def shutdown(self):
         pass
 
-    def create_browser(self, identifier):
-        cef_browser = CefBrowser()
+    def create_browser(self, identifier, url):
+        cef_browser = CefBrowser(url=url)
         self.browsers[identifier] = cef_browser
         return cef_browser
 
     def get_browser(self, identifier):
         return self.browsers[identifier]
+
+    @staticmethod
+    def check_versions():
+        print("[cef_manager.py] CEF Python {ver}".format(ver=cef.__version__))
+        print("[cef_manager.py] Python {ver} {arch}".format(ver=platform.python_version(), arch=platform.architecture()[0]))
+        assert cef.__version__ >= "55.3", "CEF Python v55.3+ required to run this"
 
     @staticmethod
     def get_native_cef():
@@ -31,5 +42,5 @@ class CefManager:
 
 class CefBrowser:
 
-    def __init__(self):
-        self.nativeBrowser = cef.CreateBrowserSync()
+    def __init__(self, url):
+        self.nativeBrowser = cef.CreateBrowserSync(url=url)
